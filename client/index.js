b63bece926342473863aa0e40748fa57c78983de
ahsanu123 <caasperahsanuamala5@gmail.com>
2022-08-14 22:19:42 -0700

    backend untuk create docx sudah bisa

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
 import { 
 Breadcrumb, 
 Layout, 
@@ -8,42 +9,80 @@ Input,
 Form
 } from 'antd';
 
+import {
+DesktopOutlined,
+FileOutlined,
+PieChartOutlined,
+TeamOutlined,
+UserOutlined,
+HeartFilled,
+}from '@ant-design/icons';
+
 import 'antd/dist/antd.css';
 
 const { Header, Content, Footer, Sider } = Layout;
 const { TextArea } = Input;
 
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem('Proposal Trovi', '0', <HeartFilled />),
+  getItem('Laporan Magang', '1', <HeartFilled />),
+  getItem('Rekomendasi Magang', '2', <HeartFilled />),
+  
+];
 
-const App = () =>{
-	const [collapsed, setCollapsed] = useState(false);
-	return (
-		
-		<Layout>
-			<Header>Header</Header>
-			<Layout>
-				<Sider>Sider</Sider>
-				<Content>
-				
-					<Form
-					labelCol={{
-					  span: 4,
-					}}
-					wrapperCol={{
-					  span: 14,
-					}}
-					layout="horizontal"
-				    >
-					<Form.Item label="TextArea">
-					  <TextArea rows={4} />
-					</Form.Item>
-					</Form>
-				
-				</Content>
-				<Footer>Footer</Footer>
-			</Layout>
-        </Layout>
-	);
+const data = {
+	"judul": "LAPORAN PERTANGGUNGJAWABAN KEGIATANELEKTRO VIRTUAL (TROVI)",
+	"detailJudul": "BIDANG IV DEPARTEMEN KADERISASI DAN INFORKOM\nSENAT MAHASISWA FAKULTAS\nFAKULTAS TEKNIK ELEKTRONIKA DAN KOMPUTER\nUNIVERSITAS KRISTEN SATYA WACANA\nSALATIGA\n2022",
+	"isi": "lorem ipsum dolor sit amet abc de fg hij 12345 1234 4 4 4!!!! 2 !@($*&@^(@)@*",
+	
 };
 
-ReactDOM.render( <App/>, document.getElementById('root')
-);
+const App = () =>{
+  // variable
+  const [collapsed, setCollapsed] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('1');
+  
+  // function 
+  const changeMenu = (menu) =>{
+	  setSelectedOption(menu.key);
+	  data["judul"]= "google"+menu.key;
+	  console.log(data["judul"]);
+  }
+  return (
+    <Layout>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <Menu 
+		theme="dark" 
+		defaultSelectedKeys={['1']} 
+		mode="inline" 
+		items={items} 
+		onClick={changeMenu}
+		/>
+      </Sider>
+	  
+	  <Layout className="site-layout">
+		<Content>
+		  <p>Selected menu: {selectedOption}, {items[selectedOption].label}</p>
+		</Content>
+	    <Footer
+          style={{
+            textAlign: 'left',
+          }}
+        >
+          Made With  ❤️ By Ah...
+        </Footer>
+	  </Layout>
+      
+    </Layout>
+  );
+};
+
+ReactDOM.render( <App/>, document.getElementById('root'));
--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const app = express()
+import * as fs from 'fs';
+import { TemplateHandler } from 'easy-template-x';
+import express from 'express';
+import path from 'path';
+import FileSaver from 'file-saver';
 
-app.use(express.static('dist'))
+const app = express();
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+const pathInput = path.resolve("contohDocx","lpjKegiatan.docx");
+const pathOutput = path.resolve("contohDocx","output.docx")
+const templateFile = fs.readFileSync(pathInput);
+
+const data = {
+	"judul": "LAPORAN PERTANGGUNGJAWABAN KEGIATANELEKTRO VIRTUAL (TROVI)",
+	"detailJudul": "BIDANG IV DEPARTEMEN KADERISASI DAN INFORKOM\nSENAT MAHASISWA FAKULTAS\nFAKULTAS TEKNIK ELEKTRONIKA DAN KOMPUTER\nUNIVERSITAS KRISTEN SATYA WACANA\nSALATIGA\n2022",
+	"isi": "lorem ipsum dolor sit amet abc de fg hij 12345 1234 4 4 4!!!! 2 !@($*&@^(@)@*",
+	
+};
+
+app.get('/api', async(req, res)=>{
+	
+	const handler = new TemplateHandler();
+	const doc = await handler.process(templateFile, data);
+	fs.writeFileSync(pathOutput, doc);
+	//res.send(pathOutput );
+	res.download(pathOutput);
+});
+app.use(express.static('dist'));
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
